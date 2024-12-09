f390c613ea599e6e58b9e8e581853856f72b545d
SarkariKill <149323638+SarkariKill@users.noreply.github.com>
2024-12-09 19:22:10 +0530

    Moo

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -25,9 +25,10 @@ SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 
 
-# AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
-# AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
-# REGION_NAME = "us-east-1"
+AWS_ACCESS_KEY_ID_ENV_KEY = os.getenv("AWS_ACCESS_KEY_ID")
+
+AWS_SECRET_ACCESS_KEY_ENV_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+REGION_NAME = "ap-south-1"
 
 
 
@@ -59,23 +60,23 @@ DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
 
 
-# """
-# MODEL TRAINER related constant start with MODEL_TRAINER var name
-# """
-# MODEL_TRAINER_DIR_NAME: str = "model_trainer"
-# MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
-# MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
-# MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
-# MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
+"""
+MODEL TRAINER related constant start with MODEL_TRAINER var name
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
 
 
-# """
-# MODEL EVALUATION related constant 
-# """
-# MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
-# MODEL_BUCKET_NAME = "usvisa-model2024"
-# MODEL_PUSHER_S3_KEY = "model-registry"
+"""
+MODEL EVALUATION related constant 
+"""
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "maito-gai-usvia"
+MODEL_PUSHER_S3_KEY = "model-registry"
 
 
-# APP_HOST = "0.0.0.0"
-# APP_PORT = 8080
+APP_HOST = "0.0.0.0"
+APP_PORT = 8080
